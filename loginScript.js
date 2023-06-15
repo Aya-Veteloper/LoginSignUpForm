c289491895e26d9037b7f10860ce2aa1aba32f03
aya-m-98 <106470807+aya-m-98@users.noreply.github.com>
2023-06-15 10:56:49 +0200

    login

---
+++ loginScript.js
@@ -0,0 +1,40 @@
+// Get the form elements
+const userNameInput = document.querySelector('.userName input');
+const passwordInput = document.querySelector('.password input');
+const signInButton = document.querySelector('.signUp button');
+
+// Function to handle form submission
+const signIn = (event) => {
+  event.preventDefault(); // Prevent form submission
+
+  // Retrieve user input values
+const userName = userNameInput.value.trim();
+const password = passwordInput.value;
+
+  // Perform authentication or validation logic here
+  // You can check against stored user data or send an API request for authentication
+
+  // Example authentication logic (replace with your own logic)
+const storedUserData = localStorage.getItem('userData');
+if (storedUserData) {
+    const userData = JSON.parse(storedUserData);
+    if (userName === userData.userName && password === userData.password) {
+      // Successful sign-in
+    alert('Sign in successful!');
+      // Perform any further actions (e.g., redirect to a new page)
+    } else {
+      // Invalid credentials
+    alert('Invalid username or password. Please try again.');
+    }
+} else {
+    // No user data found
+    alert('No user data found. Please sign up first.');
+}
+
+  // Clear the form inputs
+userNameInput.value = '';
+passwordInput.value = '';
+};
+
+// Add event listener to the "Sign In" button
+signInButton.addEventListener('click', signIn);
